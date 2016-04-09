2e0f655884e43f19c592d0eff798673074ecbe76
kersseliM <ttk14smarkoj@kamk.fi>
2016-04-09 21:50:27 +0300

    Iso kasa paskaa

---
+++ RayGameJam/Assets/Jekku.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jekku : MonoBehaviour {
+
+    public float todenn= 4;
+    SpriteRenderer sp;
+    bool check;
+	// Use this for initialization
+	void Start () {
+
+        sp = GetComponent<SpriteRenderer>();
+        Invoke("c", Random.Range(0, 20));
+	}
+
+
+    void c()
+    {
+        check = true;
+        if (Random.Range(0, 2000) >= todenn)
+        {
+            sp.enabled = true;
+        }
+    }
+
+
+}
