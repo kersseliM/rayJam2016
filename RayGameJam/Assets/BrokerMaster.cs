1f853609e9e03f95660a63e07f1f9e5c4ed1c63f
kersseliM <ttk14smarkoj@kamk.fi>
2016-04-10 12:02:12 +0300

    KuolemaEffecti

---
+++ RayGameJam/Assets/BrokerMaster.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BrokerMaster : MonoBehaviour
+{
+    
+
+  public  MeneRikki[] pelaajienHaamut;
+
+    void Start()
+    {
+        
+        
+    }
+
+    public void Broke(int id)
+    {
+        pelaajienHaamut[id].BrokeMe();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ RayGameJam/Assets/MeneRikki.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeneRikki : MonoBehaviour
+{
+
+    public GameObject eiRikkiOleva;
+   Transform[] palaset;
+
+    // Use this for initialization
+    void Start()
+    {
+        palaset = new Transform[transform.childCount];
+        print(transform.childCount);
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            palaset[i] = transform.GetChild(i);
+        }
+
+    }
+
+
+    public void BrokeMe()
+    {
+
+      
+        eiRikkiOleva.gameObject.SetActive (false);
+
+        foreach(Transform t in palaset)
+        {
+            t.gameObject.SetActive(true);
+        }
+
+
+    }
+
+}
