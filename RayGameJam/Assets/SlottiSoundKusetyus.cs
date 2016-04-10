9bf63c746406642534137a6ace9aeb343323d497
kersseliM <ttk14smarkoj@kamk.fi>
2016-04-10 10:49:36 +0300

    Slotti kusetus

---
+++ RayGameJam/Assets/SlottiSoundKusetyus.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlottiSoundKusetyus : MonoBehaviour {
+
+    public float itime;
+	// Use this for initialization
+	void Start () {
+        Invoke("i", itime);
+	}
+
+
+    void i()
+    {
+        GetComponent<AudioSource>().Play();
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
